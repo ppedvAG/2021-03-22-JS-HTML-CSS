e907fab171d1e46d4976e6f55a1486f38ee0d8cd
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-03-22 16:55:34 +0100

    tag 1 16:55

--- TRAINER/js-numbers.js
+++ TRAINER/js-numbers.js
@@ -1,20 +1,11 @@
 var alterPerson1 = 34;
 var alterPerson2 = 45;
-
+/* = ist kein Gleichheitszeichen, sondern ein Zuweisungsoperator */
 var differenz = alterPerson2 - alterPerson1;
 
 console.log(differenz);
 
-// Übung: 
-// 1. Alter von 2 Personen abfragen
-// 2. Unterschied berechnen und 
-// 3. in die Konsole ausgeben
 
-function handleClick() {
-    var person1age = age1.value;
-    console.log(age1);
-    console.log(person1age);
-}
 
 
 
