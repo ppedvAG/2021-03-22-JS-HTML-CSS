3c02c34d8d76d6ae067a38735d81545d4e6b042f
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-03-24 09:49:24 +0100

    tag 3 09:49

--- TRAINER/intro/js-intro.js
+++ TRAINER/intro/js-intro.js
@@ -1,6 +1,25 @@
-console.log('text aus js datei') // ausgabe in die Konsole
-/* d
-d
-d
-d
+// BEFEHLE
+console.log('text aus js datei')
+
+// KOMMENTARE
+// Einzeiler, z.B. ausgabe in die Konsole
+/* 
+mehrzeilige 
+Kommentare
  */
+
+// VARIABLEN
+// Definition:
+// Semantische Syntax:
+// Schlüsselwort VariablenName = VariablenWert;
+let variable3 = 345;
+let variable4 = 'text';
+
+// Aufruf einer Variable
+console.log(variable3)
+
+// FUNKTIONEN
+
+// ARRAYS
+
+// OBJEKTE
